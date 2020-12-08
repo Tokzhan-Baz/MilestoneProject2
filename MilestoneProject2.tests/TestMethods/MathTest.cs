17393a99381bf7dca9250fc68a0959b45080968b
Tokzhan-Baz <b.tokzhan24@gmail.com>
2020-12-08 22:59:40 +0600

    Milestone3

---
+++ MilestoneProject2.tests/TestMethods/MathTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MilestoneProject2.Services;
+
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MilestoneProject2.tests.TestMethods
+{
+    public class MathTest
+    {
+        [Fact]
+        public void Setup()
+        {
+
+        }
+        [Fact]
+        public int TestSum()
+        {
+            var math = new MathBL();
+            int result = math.sum(100,100);
+            return result;
+
+        }
+    }
+}
---
+++ MilestoneProject2/Services/MathBL.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MilestoneProject2.Services
+{
+    public class MathBL
+    {
+        public int sum(int num1, int num2)
+        {
+            int sum = num1 + num2;
+            return sum;
+        }
+    }
+}
